f5dbbb1e2934a96af1e17773b038d8983cfff849
YuriSerhiienko <126407379+YuriSerhiienko@users.noreply.github.com>
2023-04-17 19:13:09 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='clean_folder',
-    version='0.1.0',
+    version='0.1.1',
+    url="https://github.com/YuriSerhiienko/homework-02.git",
+    author="Yurii Serhiienko",
+    author_email="yurasergienko97@gmail.com",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'clean-folder = clean_folder.clean_folder.clean:main'
-        ]
+        "console_scripts": ["clean-folder = clean_folder.clean:run"]
     }
+
 )
